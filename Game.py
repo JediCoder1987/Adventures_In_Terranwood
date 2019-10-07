51e814fa84a6a31ff8ba3bc9121ab6418a3bc77f
JediCoder1987 <56264316+JediCoder1987@users.noreply.github.com>
2019-10-07 15:02:50 -0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,1 +1,24 @@
-print ("Hello World")
+print ("Hello! And Welcome To Adventures In Terranwood")
+
+name = input ("Please Enter Your Name Adventurer")
+
+print ("Okay, Your Name Is{} ".format (name))
+
+print ("Welcome to Terranwood, the only place where Any man can be free from all there troubles. You are a Private Investigator, and a world renown one at that! You are on the hunt for the imfamous mobster: Don Lucile Maxworth, she has many crimes to her name yet she always managed to get the not guilty verdect. That will change once you get done with her.")
+
+print ("You touch down at the airport, and take a deep breath, you look around and see a sign that reads{}. What Do You Do?".format(name))
+
+print ("1. Walk Over To The Person Holding A Sign")
+print ("2. I Don't Trust Anybody!")
+
+choice = int (input ()) 
+
+if choice== 1:
+  print ("You Walk Over To The Person, Which turned out to be an assasian for the Don. They blows up the airport with you inside. GAME OVER")
+
+elif choice== 2:
+  print ("You walk out to a taxi and have it bring you to the Police Station. once you arive you walk inside.")
+
+print ('the person at the desk looks at you "can i help you?')
+print ('1. "Hello i am{}, The private detective here to help on the Maxworth Case'.format(name))
+print ('2. "Sup"') 
